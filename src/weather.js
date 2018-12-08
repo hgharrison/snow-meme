bf0b001e3735e60e9eac66dec57f24a59d5bb367
Hank Harrison <hank_harrison@hotmail.com>
2018-12-07 18:47:50 -0600

    Updated weather.js file to reflect snow status.

--- src/weather.js
+++ src/weather.js
@@ -4,8 +4,8 @@ import {SocialIcon} from "react-social-icons";
 const Weather = (props) => {
     return(
         <header className="App-header">
-            <h1 className="App-title">{props.description &&<p>The current weather in Austin is:  {props.description}</p>}</h1>
-            <h1 className="Sub-header">Follow our live tracker for updates - <SocialIcon
+            <h1 className="App-title">{props.description &&<p>It's not looking like it's gonna snow in Austin anytime soon, chief.</p>}</h1>
+            <h1 className="Sub-header">Follow our live weather balloon for updates - <SocialIcon
                 url="http://instagram.com/hankharrison"/>
             </h1>
 
